f727c1bfec377c25b5a192854b5f939859f56f4f
Josep Curto <josep.curto@gmail.com>
2014-12-10 09:31:53 +0000

    Initial Commit

---
+++ 1. Setup - Customer Analytics.R
@@ -0,0 +1,16 @@
+###############################################################################
+## Title: Customer Analytics: Environment Setup
+## Version: 0.1
+## Autor: Josep Curto
+## Description: Packages that can be used to analyse customer data
+###############################################################################
+
+# Package installation
+install.packages("BTYD")
+install.packages("arules")
+install.packages("arulesViz")
+
+# Package load
+library(BTYD)
+library(arules)
+library(arulesViz)
---
+++ README.md
@@ -0,0 +1,9 @@
+Customer Analytics
+================
+
+Customer Analytics with R
+
+This is a repository of R links for analyzing customer data.
+
+1. [Analyze Core](http://analyzecore.com)
+
